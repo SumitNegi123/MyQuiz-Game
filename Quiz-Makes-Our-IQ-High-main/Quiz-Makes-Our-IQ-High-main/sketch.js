751c832c0edd9f5804f1047a0cad3f7b49ece739
SumitNegi123 <74806024+SumitNegi123@users.noreply.github.com>
2021-04-18 18:02:22 +0530

    Add files via upload

---
+++ Quiz-Makes-Our-IQ-High-main/Quiz-Makes-Our-IQ-High-main/sketch.js
@@ -0,0 +1,32 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  //maam can u please tell me the error line no 15 in class
+  database =firebase.database()
+  console.log(database)
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 4){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
